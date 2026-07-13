ad180f9d6e7c4ca1b647c7bb786aab3e52e3c834
7cff <me@7cff.net>
2026-07-13 14:57:42 -0700

    payments: trim whitespace

--- uber/payments.py
+++ uber/payments.py
@@ -182,7 +182,7 @@ class PreregCart:
     @property
     def has_targets(self):
         return not not self._targets
-    
+
     @property
     def purchaser(self):
         """
@@ -202,7 +202,7 @@ class PreregCart:
         for model in self.models:
             if get_age_from_birthday(model.birthdate, c.NOW_OR_AT_CON) >= 18:
                 maybe_purchasers.append(model)
-        
+
         maybe_purchasers = maybe_purchasers or [m for m in self.models]
 
         if c.ATTENDEE_ACCOUNTS_ENABLED:
@@ -721,7 +721,7 @@ class AuthNetRequestMixin:
 
     def log_authorizenet_response(self, intent_id, txn_info, card_info):
         from uber.models import ReceiptInfo, ReceiptTransaction, Session
-        
+
         session = Session().session
         matching_txns = session.query(ReceiptTransaction).filter_by(intent_id=intent_id).all()
 
@@ -731,7 +731,7 @@ class AuthNetRequestMixin:
 
         if not matching_txns:
             log.debug(f"Tried to save receipt info for intent ID {intent_id} but we couldn't find any matching payments!")
-        
+
         for txn in matching_txns:
             txn.receipt_info = ReceiptInfo(txn_info=txn_info, card_data=card_info, charged=datetime.now())
             session.add(txn.receipt_info)
