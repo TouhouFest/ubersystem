6ce2071cff53636bcdc218f4a0088d4fa883acd3
james7132 <contact@no-bull.sh>
2026-07-17 12:08:12 -0700

    feat: migrate WSGI HTTP server from Cheroot to Granian (Rust/hyper)

--- run_server.py
+++ run_server.py
@@ -1,8 +1,21 @@
 from __future__ import unicode_literals
 
+import os
 import cherrypy
 
+is_wsgi = __name__ != '__main__' or os.environ.get("GRANIAN") == "true"
+
+if is_wsgi:
+    cherrypy.config.update({'environment': 'embedded', 'engine.signals.on': False})
+    cherrypy.server.unsubscribe()
+
 import uber.server
+
+if is_wsgi:
+    cherrypy.server.unsubscribe()
+    cherrypy.engine.start()
+
+application = cherrypy.tree
 
 if __name__ == '__main__':
     cherrypy.engine.start()
