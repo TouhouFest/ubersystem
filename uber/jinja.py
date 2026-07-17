9a50c54382f68aca6d6b628e2e5ab3bc903fc97e
james7132 <contact@no-bull.sh>
2026-07-17 11:31:11 -0700

    perf: enable Jinja2 FileSystemBytecodeCache for template AST caching

--- uber/jinja.py
+++ uber/jinja.py
@@ -171,9 +171,9 @@ class JinjaEnv:
         cls.clear_cache()
 
     @classmethod
-    def env(cls):
+    def env(cls, **kwargs):
         if cls._env is None:
-            cls._env = cls._init_env()
+            cls._env = cls._init_env(**kwargs)
         return cls._env
 
     @classmethod
@@ -182,13 +182,23 @@ class JinjaEnv:
             cls._env._get_matching_filenames.cache_clear()
 
     @classmethod
-    def _init_env(cls):
+    def _init_env(cls, **kwargs):
+        cache_dir = os.path.join(getattr(c, 'DATA_DIR', '/tmp'), 'jinja_cache')
+        os.makedirs(cache_dir, exist_ok=True)
+
+        params = {
+            'loader': AbsolutePathLoader(cls._template_dirs),
+            'autoescape': True,
+            'lstrip_blocks': True,
+            'trim_blocks': True,
+            'keep_trailing_newline': True,
+            'bytecode_cache': jinja2.FileSystemBytecodeCache(directory=cache_dir, pattern='%s.cache'),
+        }
+        params.update(kwargs)
+
         env = MultiPathEnvironment(
             base_template_paths=cls._base_template_paths,
-            autoescape=True,
-            loader=AbsolutePathLoader(cls._template_dirs),
-            lstrip_blocks=True,
-            trim_blocks=True,
+            **params
         )
 
         for name, func in cls._exportable_functions.items():
